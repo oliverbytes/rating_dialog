cf934f73c70847511931b3a052102eac97ec7c1b
davidAg9 <shane.qoubby@gmail.com>
2020-10-10 21:20:04 +0000

    added comment field and button for review

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Rating Dialog Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: ExampleScreen(),
     );
@@ -20,29 +20,34 @@ class MyApp extends StatelessWidget {
 class _ExampleScreenState extends State<ExampleScreen> {
   void _showRatingDialog() {
     // We use the built in showDialog function to show our Rating Dialog
+    var commentw;
     showDialog(
         context: context,
         barrierDismissible: true, // set to false if you want to force a rating
         builder: (context) {
           return RatingDialog(
             icon: const FlutterLogo(
-                size: 100,
-                colors: Colors.red), // set your own image/icon widget
+              size: 100,
+            ), // set your own image/icon widget
             title: "The Rating Dialog",
             description:
                 "Tap a star to set your rating. Add more description here if you want.",
             submitButton: "SUBMIT",
             alternativeButton: "Contact us instead?", // optional
-            positiveComment: "We are so happy to hear :)", // optional
-            negativeComment: "We're sad to hear :(", // optional
-            accentColor: Colors.red, // optional
+            // positiveComment: "We are so happy to hear :)", // optional
+            // negativeComment: "We're sad to hear :(", // optional
+            accentColor: Colors.blue, // optional
             onSubmitPressed: (int rating) {
               print("onSubmitPressed: rating = $rating");
               // TODO: open the app's page on Google Play / Apple App Store
             },
-            onAlternativePressed: () {
-              print("onAlternativePressed: do something");
-              // TODO: maybe you want the user to contact you instead of rating a bad review
+            // onAlternativePressed: () {
+            //   print("onAlternativePressed: do something");
+            //   // TODO: maybe you want the user to contact you instead of rating a bad review
+            // },
+            onCommentPressed: (value) {
+              commentw = value;
+              print(commentw);
             },
           );
         });
